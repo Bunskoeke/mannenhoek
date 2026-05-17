c91bdeda240e3209113b0960ab58a903a61aba5e
Bunskoeke <tim@bunsko.nl>
2026-05-17 21:11:14 +0200

    blogs-data.js bijgewerkt

--- js/blogs-data.js
+++ js/blogs-data.js
@@ -5,4 +5,15 @@
 // ============================================================
 
 const BLOGS_DATA = [
+  {
+    slug: "wat-is-het-verschil-tussen-een-tennisbal-en-een-padelbal",
+    title: "Wat is het verschil tussen een tennisbal en een padelbal?",
+    excerpt: "Tennisbal of padelbal, ze lijken op elkaar maar verschillen behoorlijk. Wat maakt ze anders? Hier leg ik het je simpel uit.",
+    category: "sporten",
+    categoryLabel: "Sporten",
+    date: "2026-05-17",
+    dateFormatted: "17 mei 2026",
+    readTime: "6 min lezen",
+    image: "/images/blog/wat-is-het-verschil-tussen-een-tennisbal-en-een-padelbal.jpg"
+  },
 ];
