950b15435ea1c2bc0d844c46d4770582bc1d5857
Bunskoeke <tim@bunsko.nl>
2026-05-17 21:12:35 +0200

    blogs-data.js bijgewerkt

--- js/blogs-data.js
+++ js/blogs-data.js
@@ -6,6 +6,17 @@
 
 const BLOGS_DATA = [
   {
+    slug: "wat-is-verschil-rond-padelracket-diamond-shape-padelracket",
+    title: "Wat is het verschil tussen een rond padelracket en een diamond shape padelracket?",
+    excerpt: "Rond of diamond shape padelracket? Leer de verschillen en vind jouw ideale racket voor meer power of controle op de baan.",
+    category: "sporten",
+    categoryLabel: "Sporten",
+    date: "2026-05-17",
+    dateFormatted: "17 mei 2026",
+    readTime: "7 min lezen",
+    image: "/images/blog/wat-is-verschil-rond-padelracket-diamond-shape-padelracket.jpg"
+  },
+  {
     slug: "wat-is-het-verschil-tussen-een-tennisbal-en-een-padelbal",
     title: "Wat is het verschil tussen een tennisbal en een padelbal?",
     excerpt: "Tennisbal of padelbal, ze lijken op elkaar maar verschillen behoorlijk. Wat maakt ze anders? Hier leg ik het je simpel uit.",
